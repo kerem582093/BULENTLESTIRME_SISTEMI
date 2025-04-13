48d5cbf0fb5f9a6a0919360122e0ec088cea5593
kerem582093 <mehmetcakirr44@gmail.com>
2025-04-13 21:49:35 +0300

    Add files via upload

---
+++ Bulentlestirme sistemi(digispark).ps1
@@ -0,0 +1,40 @@
+Add-Type -AssemblyName 'System.Windows.Forms'
+
+# Masaüstündeki dosya sayısını kontrol et
+$desktopPath = [Environment]::GetFolderPath('Desktop')
+$fileCount = (Get-ChildItem -Path $desktopPath).Count
+
+# 40 klasör oluşturmayı hedefle
+$maxFolders = 250
+
+# Klasörleri oluştur
+for ($i = 1; $i -le $maxFolders; $i++) {
+    $folderName = "$desktopPath\BULENTSAHIN$i"
+    
+    # Eğer klasör zaten varsa, bir sonraki klasörü oluştur
+    if (-not (Test-Path -Path $folderName)) {
+        New-Item -ItemType Directory -Path $folderName
+    }
+}
+
+# Masaüstü arka planını ayarlamak için URL'yi indir ve ayarla
+$url = "https://i.imgur.com/WnU0WbA.jpeg"
+$path = "$env:TEMP\wall.jpg"
+Invoke-WebRequest $url -OutFile $path
+
+# Arka planı değiştirmek için gerekli API çağrısını yap
+Add-Type '[DllImport("user32.dll")]public static extern bool SystemParametersInfo(int u,int p,string s,int f);' -Name a -Namespace b
+[b.a]::SystemParametersInfo(20,0,$path,3)
+
+# URL'yi aç
+Start-Process 'https://i.imgur.com/oFJb19f.png'
+
+# Video URL'sini aç
+Start-Sleep -Seconds 2 # Video URL'sinden önce 2 saniye bekle
+Start-Process 'https://www.youtube.com/watch?v=tGjylPp45hk'
+
+# Mesaj kutularını 30 kez göster
+Start-Sleep -Seconds 3 # Video ve resim açıldıktan sonra biraz bekle
+for ($j = 1; $j -le 30; $j++) {
+    [System.Windows.Forms.MessageBox]::Show('BULENTLESTIRME ISLEMI TAMAMLANDI', 'İşlem Tamamlandı', [System.Windows.Forms.MessageBoxButtons]::OK, [System.Windows.Forms.MessageBoxIcon]::Information)
+}
